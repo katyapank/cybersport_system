de88ba75e4bafec14644819429b24fd324671cde
mirana <golovanshalom@gmail.com>
2023-11-21 23:23:49 +0400

    skeleton of crud service

---
+++ cybersport-crud-service/src/main/java/com/cybersport/cybersportcrudservice/CybersportCrudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cybersport.cybersportcrudservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersportCrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybersportCrudServiceApplication.class, args);
+	}
+
+}
---
+++ cybersport-crud-service/src/test/java/com/cybersport/cybersportcrudservice/CybersportCrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersport.cybersportcrudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersportCrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
